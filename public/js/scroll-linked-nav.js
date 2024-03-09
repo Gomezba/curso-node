af47ed905dd0c15ad62d576a5bc525a99873113f
gomezba <joel_gomezba@hotmail.com>
2024-03-09 01:36:04 -0600

    Nuevas secciones

--- public/js/scroll-linked-nav.js
+++ public/js/scroll-linked-nav.js
@@ -29,3 +29,22 @@ if (submenu) {
 
 	articles.forEach((article) => observer.observe(article))
 }
+
+// addEventListener('DOMContentLoaded', () => {
+// 	const linkActive = document.querySelector('.active')
+// 	if (linkActive) {
+// 		linkActive.scrollIntoView({ behavior: 'smooth', block: 'start' })
+// 	}
+// })
+
+document.addEventListener('DOMContentLoaded', function () {
+	// Obtener el enlace activo
+	const linkActive = document.querySelector('.active')
+	if (linkActive) {
+		// Hacer scroll suave hasta el enlace activo dentro del contenedor del menú
+		const mainMenu = document.querySelector('.sidebar')
+		if (mainMenu) {
+			mainMenu.scrollTop = linkActive.offsetTop - mainMenu.offsetTop
+		}
+	}
+})
